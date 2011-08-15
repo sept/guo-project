a036ce8022d4a4a94e344652b8dc59a27e28ccc4
sept <jfsgj@163.com>
2011-08-15 16:01:30 +0800

    文件输出

--- management.c
+++ management.c
@@ -14,15 +14,63 @@ typedef struct student
 **********************/
 void menu()
 {
-    printf("\n");
+    printf("*****学生管理系统******\n");
     printf("请选择操作\n");
     printf("1 : 打印所有信息\n");
     printf("2 : 添加新的信息\n");
     printf("3 : 删除信息\n");
     printf("4 : 保存修改\n");
     printf("5 : 退出系统\n");
-    printf("\n");
+    printf("***********************\n");
 
+}
+/**************************************
+函数：read_data();
+功能：读取文件中存储以链表形式存在的内容
+返回值：head
+***************************************/
+STU *read_data()
+{
+    STU *head = NULL;
+    STU *p = NULL;
+    int num;                             /* 定义同 结构体中相同的类型 通过这几个变量 输出内容*/
+    char iname[20];
+    FILE *fp = fopen("file.txt", "r");   /*以 r 形式打开文件  即可*/
+    if (NULL == fp)                      /*是否打开文件失败*/
+    {
+        perror("em.");
+        exit(0);
+    }
+
+    if (fscanf(fp, "%d%s", &num, iname) == EOF)   /*是否文件为空*/
+    {
+        return NULL;
+    }
+
+    head = p = malloc(sizeof(STU));                /*不为空的前提下 开辟结构体类型空间*/
+    if (head == NULL)
+    {
+        perror("empty.");
+        exit(0);
+    }
+    head->num = num;                               /*先将第一个结点的数据 赋给本此函数定义的变量 */
+    strncpy(head->name, iname, 19);
+    head->next = NULL;                              
+
+    while (fscanf(fp, "%d%s", &num, iname) != EOF)  /*判断每次取完结点 是否为文件的结尾*/
+    {
+        p->next = malloc(sizeof(STU));              /*给下一个结点开辟空间*/
+        if (p->next == NULL)
+        {
+            perror("empty");
+            exit(0);
+        }
+        p->next->num = num;                        /*如第一个结点一样 将下来的结点内容赋给变量*/
+        strncpy(p->next->name, iname, 19);
+        p->next->next = NULL;
+        p = p->next;                                /*移动指针*/
+    }
+    return head;                                    /*指针返回*/
 }
 /***********************
 函数：print_node()
@@ -78,9 +126,9 @@ STU *add_node(STU *p)
 /*判断下面两个条件 从左至右*/
     while ((p->next != NULL) && (p->next->num < new->num))      
     {
-        new = new->next;                           /*移动指针*/
+        p = p->next;                                   /*移动指针*/
     }
-    new->next = p->next;
+    new->next = p->next;              /*while  语句循环结束 退出  即定位添加位置*/
     p->next = new;
 
     return head;
@@ -153,6 +201,8 @@ int main(int argc, const char *argv[])
     char choice = 0;
     int flag = 1;
     STU *head = NULL;
+
+    head = read_data();
 
     while (flag != 0)
     {   
