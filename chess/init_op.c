14e3d5b5ff29266676dfabbcbe63596c84daa4cf
sept <jfsgj@163.com>
2011-08-18 09:00:27 +0800

    初始化屏幕

--- chess/init_op.c
+++ chess/init_op.c
@@ -6,7 +6,7 @@
 #include <linux/fb.h>
 #include <sys/mman.h>
 #include "func.h"
-
+/*在 .c 文件里面 定义 声明过的变量*/
 fbscr_t fb_v;
  int mx;
  int my;
@@ -24,13 +24,13 @@ int init_data()
     if (fd < 0)
     {
     	perror("open fb0");
-	exit(0);
+	    exit(0);
     }
 
     if (ioctl(fd, FBIOGET_VSCREENINFO, &fb_var) < 0)
     {
     	perror("ioctl");
-	exit(0);
+	    exit(0);
     }
 //    printf("w = %d\t h = %d\t dpp = %d\n", fb_var.xres, fb_var.yres, fb_var.bits_per_pixel);
     fb_v.w = fb_var.xres;
@@ -39,10 +39,10 @@ int init_data()
 
     fb_v.memo = mmap(NULL, fb_v.w*fb_v.h*fb_v.bpp/8, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 
-    if (fb_v.memo == MAP_FAILED)
+    if (fb_v.memo == MAP_FAILED)            /*映射*/
     {
     	perror("map");
-	exit(0);
+	    exit(0);
     }
     memset(fb_v.memo, 0, fb_v.w*fb_v.h*fb_v.bpp/8);       /*清屏*/
 
